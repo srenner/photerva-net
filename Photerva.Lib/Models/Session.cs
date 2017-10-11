c97f3d9e2055252f99ecf624faf8ba3c87fa6564
Shawn Renner <resume@shawnrenner.com>
2017-10-10 21:59:48 -0500

    start defining Session

--- Photerva.Lib/Models/Session.cs
+++ Photerva.Lib/Models/Session.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace Photerva.Lib.Models
 {
-    class Session
+    public class Session
     {
+        public int SessionID { get; set; }
+
+        public int SessionTypeID { get; set; }
+        public SessionType SessionType { get; set; }
+
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+
+        public decimal QuotedPrice { get; set; }
+        public decimal FinalPrice { get; set; }
+
+        public TimeSpan ShootTime { get; set; }
+        public TimeSpan EditTime { get; set; }
+
+        public DateTime ScheduledDate { get; set; }
+        public DateTime? BackupDate { get; set; }
+
+        public string Notes { get; set; }
+        public bool Deleted { get; set; }
+
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
     }
 }
